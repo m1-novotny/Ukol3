9e782c441a53491944e1ad263908c20974ef0a13
m1_novotny <m1_novotny@utb.cz>
2022-12-10 14:35:37 +0100

    MarsApiService.kt - implemented an interface and an object to initialize the Retrofit service

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -27,4 +27,12 @@ private val retrofit = Retrofit.Builder()
     .addConverterFactory(ScalarsConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
-
+interface MarsApiService {
+    @GET("realestate")
+    fun getProperties():
+            Call<String>
+}
+object MarsApi {
+    val retrofitService : MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java) }
+}
